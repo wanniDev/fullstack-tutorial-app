5b2dd6d35665530aa3c044a73913ff16e0a46b4d
wanniDev <wannidev0928@gmail.com>
2022-10-08 01:17:14 +0900

    project init

---
+++ src/main/java/com/auth0/flickr2/config/ApplicationProperties.java
@@ -0,0 +1,16 @@
+package com.auth0.flickr2.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Flickr 2.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+    // jhipster-needle-application-properties-property
+    // jhipster-needle-application-properties-property-getter
+    // jhipster-needle-application-properties-property-class
+}
